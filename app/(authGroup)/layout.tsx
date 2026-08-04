959b05388a04f454a82ef0ca15c03915c36893a2
rejaur2287 <rezaurewubd@gmail.com>
2026-08-04 21:43:24 +0600

    Creating Login Form With ShadCn

---
+++ app/(authGroup)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            {children}
+        </div>
+    )
+}
+
+export default DashboardLayout

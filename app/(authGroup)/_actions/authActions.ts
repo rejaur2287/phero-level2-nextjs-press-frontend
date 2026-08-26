0fa66bd0bf0459c6ce3aeb8b120d886f1ea3ce36
rejaur2287 <rezaurewubd@gmail.com>
2026-08-26 13:58:54 +0600

    Redirecting Users To Role Based Routes After Successful Login

--- app/(authGroup)/_actions/authActions.ts
+++ app/(authGroup)/_actions/authActions.ts
@@ -2,6 +2,7 @@
 
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 type LoginState = {
   success: true;
@@ -50,7 +51,16 @@ export const loginAction = async (
       maxAge: 60 * 60 * 24 * 7,
       sameSite: "lax",
     });
-    redirect("/dashboard");
+
+    const decodedToken = jwt.decode(result.data.accessToken) as JwtPayload;
+
+    if (decodedToken.role === "USER") {
+      redirect("/dashboard");
+    } else if (decodedToken.role === "ADMIN") {
+      redirect("/admin-dashboard");
+    } else if (decodedToken.role === "AUTHOR") {
+      redirect("/author-dashboard");
+    }
   }
   return result;
 };

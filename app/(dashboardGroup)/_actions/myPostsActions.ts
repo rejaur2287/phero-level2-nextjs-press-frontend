6785f7aa7b966457aeb5153cc85b7c5c5a39da56
rejaur2287 <rezaurewubd@gmail.com>
2026-09-19 12:00:32 +0600

    Implementing Functionalities Of My Posts Page

---
+++ app/(dashboardGroup)/_actions/myPostsActions.ts
@@ -0,0 +1,36 @@
+"use server";
+import { cookies } from "next/headers";
+
+export const getMyPosts = async () => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value || null;
+
+  if (!accessToken) {
+    // throw new Error("User not Logged In!");
+    return {
+      success: false,
+      message: "User not Logged In!",
+    };
+  }
+
+  const res = await fetch(`${process.env.BACKEND_API_URL}/api/posts/my-posts`, {
+    headers: {
+      //   Authorization: accessToken as unknown as string,
+      //   Authorization: `${accessToken}`,
+      //   Authorization: `Bearer${accessToken}`,
+
+      Cookie: `accessToken = ${accessToken}`,
+    },
+    cache: "force-cache",
+    next: {
+      revalidate: 60 * 60 * 24,
+      tags: ["my-posts"],
+    },
+  });
+
+  const result = res.json();
+
+  // console.log(result);
+
+  return result;
+};
